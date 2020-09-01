81f123eb31416fd7e19c7a5f352de4f1f9d0a9fe
eim5178 <eim5178@psu.edu>
2020-09-01 18:29:44 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include<readline/readline.h>
+
+int main(void) {
+  char *celsius = (readline("Enter your name: " );
+  double farenheit = 
+  printf("Hello, %s!\f",farenheit);
+}
