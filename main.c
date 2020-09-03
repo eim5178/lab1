42bfceeb53397bd67de7cbbf178d1fb8d652b50a
eim5178 <eim5178@psu.edu>
2020-09-03 13:56:46 +0000

    °

--- main.c
+++ main.c
@@ -12,5 +12,5 @@ int main(void) {
   double temp = atof(celsius);
   double fahrenheit_temp;
   fahrenheit_temp = (temp * 9/5) + 32;
-  printf("%f°in Celsius is equivalent to %f Fahrenheit.\f", temp, fahrenheit_temp);
+  printf("%f° in Celsius is equivalent to %f° Fahrenheit.\f", temp, fahrenheit_temp);
 }
